f47f1103130f9d131f87bc8c6f87d5ba2b27db0f
Jon Goldberg <jon@megaphonetech.com>
2018-09-05 16:27:41 -0400

    Don't show addresses on mouseover or when adding a new address

--- js/hiddenaddresses.js
+++ js/hiddenaddresses.js
@@ -2,7 +2,7 @@
 
 CRM.$(function($) {
     $('div.crm-contact-page').each(function() {
-        $("<div class='crm-clear crm-summary-row'><div class='crm-content'><input type='checkbox' name='hiddenAddresses' id='show-hide-address'><span class='crm-content'>No Hidden Address(es).</span><div>").insertBefore($('div.crm-address-block div.add-new'));
+        $("<div class='crm-clear crm-summary-row'><div class='crm-content'><input type='checkbox' name='hiddenAddresses' id='show-hide-address'><span class='crm-content'>No Hidden Old Address(es).</span><div>").insertBefore($('div.crm-address-block div.add-new'));
         var checkbox = $('#show-hide-address');
         checkbox.hide();
         checkbox.next().hide();
@@ -18,7 +18,7 @@ CRM.$(function($) {
             var checkbox = $('#show-hide-address');
             checkbox.show();
             checkbox.next().show();
-            checkbox.next().text(" Show " + hiddenAddresses + " Hidden Address(es).")
+            checkbox.next().text(" Show " + hiddenAddresses + " Hidden Old Address(es).")
         }
     });
 
@@ -49,52 +49,12 @@ CRM.$(function($) {
         });
         if (hiddenAddresses > 0) {
             if ($('#show-hide-address').length === 0) {
-                $("<div class='crm-clear crm-summary-row'><div class='crm-content'><input type='checkbox' name='hiddenAddresses' id='show-hide-address'><span class='crm-content'>No Hidden Address(es).</span><div>").insertBefore($('div.crm-address-block div.add-new'));
+                $("<div class='crm-clear crm-summary-row'><div class='crm-content'><input type='checkbox' name='hiddenAddresses' id='show-hide-address'><span class='crm-content'>No Hidden Old Address(es).</span><div>").insertBefore($('div.crm-address-block div.add-new'));
             }
             var checkbox = $('#show-hide-address');
             checkbox.show();
             checkbox.next().show();
-            checkbox.next().text(" Show " + hiddenAddresses + " Hidden Address(es).")
-            checkbox.prop( "checked", true );
-        }
-    });
-
-    $('div.crm-address-block div.crm-inline-block-content').on('click', function() {
-        var hiddenAddresses = 0;
-        $('div.crm-address-block div.crm-address_type_Old').each(function() {
-            if ($(this).find('div.primary').length === 0) {
-                $(this).show();
-                hiddenAddresses = hiddenAddresses + 1;
-            }
-        });
-        if (hiddenAddresses > 0) {
-            if ($('#show-hide-address').length === 0) {
-                $("<div class='crm-clear crm-summary-row'><div class='crm-content'><input type='checkbox' name='hiddenAddresses' id='show-hide-address'><span class='crm-content'>No Hidden Address(es).</span><div>").insertBefore($('div.crm-address-block div.add-new'));
-            }
-            var checkbox = $('#show-hide-address');
-            checkbox.show();
-            checkbox.next().show();
-            checkbox.next().text(" Show " + hiddenAddresses + " Hidden Address(es).")
-            checkbox.prop( "checked", true );
-        }
-    });
-
-    $('div.crm-address-block div.crm-address_type_Old, div.crm-address-block div.crm-container-snippet div.crm-address_type_Old').on('mouseover', function() {
-        var hiddenAddresses = 0;
-        $('div.crm-address-block div.crm-address_type_Old').each(function() {
-            if ($(this).find('div.primary').length === 0) {
-                $(this).show();
-                hiddenAddresses = hiddenAddresses + 1;
-            }
-        });
-        if (hiddenAddresses > 0) {
-            if ($('#show-hide-address').length === 0) {
-                $("<div class='crm-clear crm-summary-row'><div class='crm-content'><input type='checkbox' name='hiddenAddresses' id='show-hide-address'><span class='crm-content'>No Hidden Address(es).</span><div>").insertBefore($('div.crm-address-block div.add-new'));
-            }
-            var checkbox = $('#show-hide-address');
-            checkbox.show();
-            checkbox.next().show();
-            checkbox.next().text(" Show " + hiddenAddresses + " Hidden Address(es).")
+            checkbox.next().text(" Show " + hiddenAddresses + " Hidden Old Address(es).")
             checkbox.prop( "checked", true );
         }
     });
